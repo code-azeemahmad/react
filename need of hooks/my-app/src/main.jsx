0a4d77157453c2f3f4cec19bd70058c5a232789e
code-azeemahmad <azeemahmadd9@gmail.com>
2026-05-01 09:32:16 +0500

    fibre algorithm, virtual dom and reconciliation

--- need of hooks/my-app/src/main.jsx
+++ need of hooks/my-app/src/main.jsx
@@ -3,8 +3,32 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render( // creates a DOM like structure for you bts just like browser dom
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+// createRoot compares main DOM and own DOM and updates(tracks) only those things that are actually updated in UI
+// Browser DOM removes the whole DOM and repaints it(page reloading)
+
+// every UI is not needed to update instantaneously to prevent intermediate updates in case of network calls
+
+
+/*
+  Fibre
+React Fiber is an ongoing reimplementation of React's core algorithm. Incremental rendering: the ability to split rendering work into chunks and spread it out over multiple frames.
+
+  reconciliation
+The algorithm React uses to diff one tree with another to determine which parts need to be changed.
+
+  update
+A change in the data used to render a React app. Usually the result of `setState`. Eventually results in a re-render.
+*/
+
+// Reconciliation is the algorithm behind what is popularly understood as the "virtual DOM." Keys used during reconciliation
+
+/*
+loop an array and return its values to lists, buttons, paragraphs. Why keys should be introduced?
+In fibre, in order to improve list performance, Diffing of lists is performed using keys. Keys should be "stable, predictable, and unique."
+*/
