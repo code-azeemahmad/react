07a492a41f3480a705598c34cca5af517358be6f
code-azeemahmad <azeemahmadd9@gmail.com>
2026-05-02 16:37:07 +0500

    useParam hook, useLoaderData hook and optimization through Loader

--- react router/my-app/src/components/Github/Github.jsx
+++ react router/my-app/src/components/Github/Github.jsx
@@ -1,32 +1,45 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
+
+const url = `https://api.github.com/users/code-azeemahmad`;
 
 function Github() {
-  const [follower, setFollower] = useState(0);
-  const [profile, setProfile] = useState(null);
+    const data = useLoaderData();
+//   const [follower, setFollower] = useState(0);
+//   const [profile, setProfile] = useState(null);
 
-  const url = `https://api.github.com/users/code-azeemahmad`;
-
-  // Triggers re-render → UI updates
-  useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        setFollower(data.followers);
-        setProfile(data.avatar_url);
-      });
-  });
+//   // Triggers re-render → UI updates
+//   useEffect(() => {
+//     fetch(url)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         setFollower(data?.followers);
+//         setProfile(data?.avatar_url);
+//       });
+//   });
+  
   return (
     <>
-      <div>The Followers are: {follower}</div>
-      <img src={profile} alt="github profile" width={500} height={500} />
+      {/* <div>The Followers are: {follower}</div>
+      <img src={profile} alt="github profile" width={500} height={500} /> */}
+      <div>The Followers are: {data?.followers}</div>
+      <img src={data?.avatar_url} alt="github profile" width={500} height={500} />
     </>
   );
 }
 
 export default Github;
 
+// eslint-disable-next-line react-refresh/only-export-components
+export const githubInfoLoader = async() => {
+    const response = await fetch(url);
+    return response.json();
+}
+
 /*
 Whenever you fetch data:
     useState → Stores data that can change and re-render UI
     useEffect → fetch on mount (Runs only once when component loads)
 */
+
+// Loader starts data fetching even before useEffect. Whenever you hovers the tab, the event is triggered
--- react router/my-app/src/main.jsx
+++ react router/my-app/src/main.jsx
@@ -6,7 +6,7 @@ import Layout from './Layout.jsx';
 import Home from './components/Home/Home';
 import About from './components/About/About.jsx';
 import Contact from './components/Contact/Contact.jsx';
-import Github from './components/Github/Github.jsx';
+import Github, { githubInfoLoader } from './components/Github/Github.jsx';
 import User from './components/User/User.jsx';
 
 // Nested Route Configuration using React Router (createBrowserRouter route tree)
@@ -43,7 +43,9 @@ const router = createBrowserRouter(
       <Route path='' element={<Home />} />
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
-      <Route path='github' element={<Github />} />
+      <Route
+      loader={githubInfoLoader} 
+      path='github' element={<Github />} />
       <Route path='user/:id' element={<User />} />
     </Route>
   )
