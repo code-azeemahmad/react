c70b6b59a3ddf1cbc2466e69eefe95be2675fd00
code-azeemahmad <azeemahmadd9@gmail.com>
2026-05-02 17:46:30 +0500

    provide context

---
+++ context api/miniContext/src/context/UserContextProvider.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import UserContext from "./UserContext";
+
+// children --> generic name pass as it is
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = React.useState(null);
+    return (
+        <UserContext.Provider value={{user, setUser}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export default UserContextProvider;
+
+// UserContext.Provider is mandatory 
+
+/* Step 1: Create Context
+   Step 2: Provide Context
+   Step 3: Consume Context */
+
+// how to access context
