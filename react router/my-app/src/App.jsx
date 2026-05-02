f7502e6694bc74d81e4b2140a2c667d8c39ab6b4
code-azeemahmad <azeemahmadd9@gmail.com>
2026-05-02 14:46:51 +0500

    assembling the components, the use of Link and NavLink and heading towards react router

--- react router/my-app/src/App.jsx
+++ react router/my-app/src/App.jsx
@@ -1,12 +1,19 @@
 // import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
-    <h1 className='bg-green-700 text-black p-4 pb-5'>React Router</h1>
+    <>
+    </>
   );
 }
 
-export default App
+export default App;
+
+/*
+Link is used in place of a tag, a tag refreshes the page
+There is no concept of refresh in react.
+NavLink provides some additional features
+*/
