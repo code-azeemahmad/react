31792a4cdf3580538034037618d06b5925a16e33
code-azeemahmad <azeemahmadd9@gmail.com>
2026-05-02 13:09:27 +0500

    setting up the project

---
+++ react router/my-app/src/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <h1 className='bg-green-700 text-black p-4 pb-5'>React Router</h1>
+  );
+}
+
+export default App
