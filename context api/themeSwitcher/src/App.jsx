11e2aecc40b98a931aabb149727c43c14db2faa4
code-azeemahmad <azeemahmadd9@gmail.com>
2026-05-02 20:12:22 +0500

    setting up the configurations

---
+++ context api/themeSwitcher/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-600 py-6 text-white text-3xl ">hello</h1>
+
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* theme button */}
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">{/* Card */}</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
