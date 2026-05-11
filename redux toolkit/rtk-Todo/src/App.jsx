24993eaf1e24dba1cffb5c91526b1d11b4fb031a
code-azeemahmad <azeemahmadd9@gmail.com>
2026-05-11 22:18:27 +0500

    redux toolkit todo app complete

--- redux toolkit/rtk-Todo/src/App.jsx
+++ redux toolkit/rtk-Todo/src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css'
+import './App.css';
+import AddTodo from './components/AddTodo';
+import Todo from './components/Todo';
 
 function App() {
 
   return (
+    // provider should be applied as in context api, implemented frequently in main.jsx
     <>
-      <h1 className=''>hello rtk</h1>
+      <h1 className=''>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todo />
     </>
   );
-}
+} 
 
 export default App
