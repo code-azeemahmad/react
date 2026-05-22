ed7d5c82b22175e7fff000a1fb11174e5a200130
code-azeemahmad <azeemahmadd9@gmail.com>
2026-05-22 18:45:51 +0500

    routing

--- projects/megaBlogApp/src/components/index.js
+++ projects/megaBlogApp/src/components/index.js
@@ -8,5 +8,6 @@ import Signup from "./Signup";
 import Login from "./Login";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout"
 
-export { Header, Footer, Container, Logo, LogoutBtn, RTE, Signup, Login, PostForm, PostCard };
+export { Header, Footer, Container, Logo, LogoutBtn, RTE, Signup, Login, PostForm, PostCard, AuthLayout };
