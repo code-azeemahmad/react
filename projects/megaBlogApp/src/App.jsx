7541a8b7849f466ce0c9db52db27e1c071e8a667
code-azeemahmad <azeemahmadd9@gmail.com>
2026-05-22 07:01:52 +0500

    configure redux toolkit in big projects

--- projects/megaBlogApp/src/App.jsx
+++ projects/megaBlogApp/src/App.jsx
@@ -1,12 +1,45 @@
-import './App.css'
+import "./App.css";
+import './index.css'
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
+  const [loading, setLoading] = useState(true); // conditional rendering
+  const dispatch = useDispatch(); // bring user
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        console.log("Auth check failed:", error);
+        dispatch(logout());
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [dispatch]);
+
+  return !loading ? (
     <>
-      <h1>Mega Blog App</h1>
+      <div className="min-h-screen flex flex-wrap content-between bg-gray-400 ">
+        <div className="w-full block">
+          <Header />
+          <main>{/* <Outlet /> */}</main>
+          <Footer />
+        </div>
+      </div>
     </>
-  )
+  ) : null;
 }
 
-export default App
+export default App;
